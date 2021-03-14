6e8cc3ed17f5872330fd923b1fa4504889a01afb
KiyoshiYamazaki <take_my_time.23@icloud.com>
2021-03-14 20:36:43 +0900

    Scond Commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Task1App' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Task1App
+
+  pod 'SwiftLint', '~> 0.41.0'
+
+  target 'Task1AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Task1AppUITests' do
+    # Pods for testing
+  end
+
+end
--- Task1App/ViewController.swift
+++ Task1App/ViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textlabel: UILabel?
+    
+    private var totalValue:Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textField1.keyboardType = UIKeyboardType.numberPad
+        textField2.keyboardType = UIKeyboardType.numberPad
+        textField3.keyboardType = UIKeyboardType.numberPad
+        textField4.keyboardType = UIKeyboardType.numberPad
+        
+        
+        
+    }
+    
+    func changeStringToInt(string:String) -> Int{
+        
+        if let num = Int(string){
+            
+            return num
+        }else{
+            
+            return 0
+        }
+        
+    }
+    
+    
+    func culculate(num1:Int,num2:Int,num3:Int,num4:Int) -> Int{
+        //合計値を算出する
+        num1 + num2 + num3 + num4
+        
     }
 
-
+    @IBAction func calculationButton(_ sender: Any) {
+        //textFieldに入力した値を合計したtotalValueをLabelに表示する
+        
+        totalValue = culculate(
+            num1: changeStringToInt(string: textField1.text ?? ""),
+            num2: changeStringToInt(string: textField2.text ?? ""),
+            num3: changeStringToInt(string: textField3.text ?? ""),
+            num4: changeStringToInt(string: textField4.text ?? ""))
+        
+        textlabel?.text = String(totalValue)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
+extension ViewController:UITextFieldDelegate{
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
