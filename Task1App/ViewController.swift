884535af152d0a5a95cbd1cb31d852e33d1b14f0
akio0911 <akio0911@gmail.com>
2021-03-19 19:47:40 +0900

    feedback

--- Task1App/ViewController.swift
+++ Task1App/ViewController.swift
@@ -13,24 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak private var textField3: UITextField!
     @IBOutlet weak private var textField4: UITextField!
     @IBOutlet weak private var textField5: UITextField!
+
     @IBOutlet weak private var textlabel: UILabel?
-    private var totalValue: Int!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        textField1.keyboardType = UIKeyboardType.numberPad
-        textField2.keyboardType = UIKeyboardType.numberPad
-        textField3.keyboardType = UIKeyboardType.numberPad
-        textField4.keyboardType = UIKeyboardType.numberPad
-        textField5.keyboardType = UIKeyboardType.numberPad
+
+        textField1.keyboardType = .numberPad
+        textField2.keyboardType = .numberPad
+        textField3.keyboardType = .numberPad
+        textField4.keyboardType = .numberPad
+        textField5.keyboardType = .numberPad
     }
+
     func changeStringToInt(string: String) -> Int {
-        if let num = Int(string) {
-            return num
-        } else {
-            return 0
-        }
+        Int(string) ?? 0
     }
+
     func culculate(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int) -> Int {
         // 合計値を算出する
         num1 + num2 + num3 + num4 + num5
@@ -38,17 +37,18 @@ class ViewController: UIViewController {
 
     @IBAction private func calculationButton(_ sender: Any) {
         // textFieldに入力した値を合計したtotalValueをLabelに表示する
-        totalValue = culculate(
+        let totalValue = culculate(
             num1: changeStringToInt(string: textField1.text ?? ""),
             num2: changeStringToInt(string: textField2.text ?? ""),
             num3: changeStringToInt(string: textField3.text ?? ""),
             num4: changeStringToInt(string: textField4.text ?? ""),
             num5: changeStringToInt(string: textField5.text ?? "")
         )
+
         textlabel?.text = String(totalValue)
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+        view.endEditing(true)
     }
 }
 
